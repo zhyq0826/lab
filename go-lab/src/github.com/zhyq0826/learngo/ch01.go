b6aa4674d9c2008e2757a425287fd3278f521633
zhyq0826 <zhyq0826@126.com>
2017-04-18 19:32:29 +0800

    update

--- go-lab/src/github.com/zhyq0826/learngo/ch01.go
+++ go-lab/src/github.com/zhyq0826/learngo/ch01.go
@@ -4,5 +4,16 @@ import "fmt"
 
 func main() {
     const s = 10
-    fmt.Printf("%d", s)
+    fmt.Printf("%d\n", s)
+
+    array := []int{1, 2, 3, 4, 5, 6}
+    for _, item := range array {
+        fmt.Printf("%d", item)
+    }
+
+    sr := "abcedf"
+    c := []rune(sr)
+    for _, i := range c {
+        fmt.Printf("%c", i)
+    }
 }
