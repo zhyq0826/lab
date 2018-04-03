757393475d3183680f6a9375de39cf44340611ac
ssss <zhyq0826@126.com>
2018-04-03 19:40:50 +0800

    update

---
+++ celery-lab/batch_task_add.py
@@ -0,0 +1,14 @@
+from celery import group, chain
+from proj.celery import app
+
+
+def main():
+    for i in range(100000):
+        app.send_task(
+            'proj.add',
+            args=(1, 2),
+        )
+
+
+if __name__ == '__main__':
+    main()
